8bf21b37999528b13934cf8a7c2b97ba62e52dfb
Cookie9467 <76694912+Cookie9467@users.noreply.github.com>
2025-05-16 00:04:07 +0800

    Update PlayerColor.java
    Create OPPONENT_MAP

--- src/core/model/PlayerColor.java
+++ src/core/model/PlayerColor.java
@@ -1,5 +1,7 @@
 package core.model;
 
+import java.util.Map;
+
 public enum PlayerColor {
     RED,
     BLUE,
@@ -7,5 +9,18 @@ public enum PlayerColor {
     YELLOW,
     PURPLE,
     ORANGE,
-    NONE // 用於中間六角形或空格
+    NONE; // 用於中間六角形或空格
+
+    private static final Map<PlayerColor, PlayerColor> OPPONENT_MAP = Map.of(
+            RED, YELLOW,
+            YELLOW, RED,
+            BLUE, ORANGE,
+            ORANGE, BLUE,
+            GREEN, PURPLE,
+            PURPLE, GREEN
+    );
+
+    public static PlayerColor getOpponent(PlayerColor color) {
+        return OPPONENT_MAP.getOrDefault(color, NONE);
+    }
 }
