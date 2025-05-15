38e19093de6724a61bb2114ff301240db561cc26
Cookie9467 <76694912+Cookie9467@users.noreply.github.com>
2025-05-16 00:03:54 +0800

    Update BoardState.java
    Create isInOpponentRegion

--- src/core/model/BoardState.java
+++ src/core/model/BoardState.java
@@ -4,5 +4,6 @@ public interface BoardState {
 
     boolean isValidPosition(Position pos);
     boolean isOccupied(Position pos);
-
+    // 是否非對家區域
+    boolean isInOpponentRegion(PlayerColor curPieceColor, Position targetPos);
 }
